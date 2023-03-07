5e3914ee080960465e75fe1a95d6f275ad2ccd96
Cristina Geremia <112133093+cgeremia@users.noreply.github.com>
2023-03-06 23:42:14 -0400

    delete unwanted directories from repo

---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar.js";
+
+const Home = () => {
+  return;
+  <div>
+    <Navbar />
+  </div>;
+};
+
+export default Home;
+
+
+test12345
